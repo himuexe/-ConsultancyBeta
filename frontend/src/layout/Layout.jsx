5a5fa820ca7fe23cd0f0aa9a2f820e492206285b
Himanshu Sharma <sharmaslov@gmail.com>
2025-02-04 13:57:38 +0530

    Fix

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Footer from "../components/Footer.jsx";
+import Footer from "../components/footer.jsx";
 import Nav from "../components/Nav.jsx";
 
 const Layout = ({children}) => {
