6c2c0b09d9d86e2e2526c621499af39212b4eed3
Himanshu Sharma <sharmaslov@gmail.com>
2025-02-04 14:12:42 +0530

    Update Homepage.jsx

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,10 @@
 import Hero from '../components/Hero'
 import Services from '../components/services'
 import SubServices from '../components/SubServices'
-import AboutUs from '../components/AboutUs'
+import AboutUs from '../components/aboutUs'
 import Intersngrid from '../components/cards'
 import QuoteCarousel from '../components/Carousel'
-import Tech from '../components/tech'
+import Tech from '../components/Tech'
 import Pricing from '../components/Pricing'
 const Homepage = () => {
   return (
